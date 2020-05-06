b796fb3909d9c8c43c742d5d69d5af6def96e0ce
DROZD-01 <sasha.bogdanov87@gmail.com>
2020-05-06 22:44:43 +0300

    now gitignore surely works

---
+++ lab2/project_lab_2/rtl/hub.v
@@ -0,0 +1,68 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 27.03.2020 23:26:34
+// Design Name: 
+// Module Name: KEY_Switching
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+//Модуль hub 
+//Всё назначение данного модуля - соединять несколько подмодулей. при желании код можно переделать, и все сигналы соединять в mainframe
+module hub(               //модуль hub (префикс - "KS")
+input   [9:0]   sw_i,              //Ввод переключателей
+input   [4:0]   btn_i,             //Ввод кнопок 
+input           clk_50m,        //Ввод тактового сигнала частотой 50 МГц
+output  [9:0]   register_o,        //Вывод регистра
+output  [7:0]   counter_o          //Вывод счётчика
+    );
+  
+  wire          synced_event;    //Ввод сигнала о произошедшем УС
+  wire  [4:0]   btn_ondn;        //Ввод сигнала о том, что кнопка была НАЖАТА
+  
+ //Ввод/вывод сигналов от модуля KEY_Pressing (префикс - "KP")
+  keys_debounce u1(
+    .btn_i(btn_i[4:0]),
+    .clk_50m(clk_50m),
+    .btn_ondn_o(btn_ondn[4:0])
+  ); // Ввод кнопок
+  
+  //Ввод/вывод сигналов от модуля Register_10 (регистр на 10 позиций)
+  register_10 u2(
+    .d_i(sw_i[9:0]),                 
+    .clk_i(clk_50m),              
+    .rst_i(btn_ondn[3]),           
+    .en_i(btn_ondn[0]),            
+    .register_o(register_o[9:0])     
+  );
+  
+  //Ввод/вывод сигналов от модуля Counter_8 (8-битный счётчик)
+  counter_8 u3(
+    .clk_i(clk_50m),
+    .rst_i(btn_ondn[3]),
+    .en_i(synced_event),
+    .counter_o(counter_o[7:0])
+  ); 
+  
+  //Ввод/вывод сигналов от модуля REG_Event (префикс - "REG")(проверка наличия УС)
+  switch_event u4(
+    .sw_i(sw_i[9:0]),
+    .clk_50m(clk_50m),
+    .synced_event_o(synced_event),
+    .btn_sync_i(btn_ondn[0])
+  );
+  
+  
+endmodule
