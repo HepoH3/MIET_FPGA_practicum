9a97cd93749d3efb3851ccca3f4c2021e927be98
Max-Klenyshev <62945685+Max-Klenyshev@users.noreply.github.com>
2020-05-13 20:39:02 +0300

    Update Counter_8.v

--- lab2/lab_2/lab_2.srcs/sources_1/new/Counter_8.v
+++ lab2/lab_2/lab_2.srcs/sources_1/new/Counter_8.v
@@ -1,9 +1,9 @@
 `timescale 1ns / 1ps
 module Counter_8(
-    input            clk_i,       //Тактовый импульс
-    input            en_i,        //Сигнал на включение счётчика (произошло УС)
-    input            rst_i,       //Сброс счётчика в 0
-    output reg [7:0] counter_o    //Вывод значения счётчика (8 бит)
+    input            clk_i,     
+    input            en_i,       
+    input            rst_i,       
+    output reg [7:0] counter_o   
     );
     
     always @(posedge clk_i or posedge rst_i) begin
