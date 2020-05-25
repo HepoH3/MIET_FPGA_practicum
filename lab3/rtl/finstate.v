117f24e6c919a4e9d165c4059b5dd642cd2eae0f
Enot4228 <62872569+Enot4228@users.noreply.github.com>
2020-05-25 19:02:20 +0300

    Добавление автомата

---
+++ lab3/rtl/finstate.v
@@ -0,0 +1,112 @@
+timescale 1ns / 1ps
+
+module finstate(
+  input        clk_i,
+  input        rstn_i,
+  input        dev_run_i,
+  input        set_i,
+  input        start_i,
+  input        change_i,
+  output [2:0] state_value_o,
+  output       inc_this_o,
+  output [3:0] cnt
+);
+
+reg [2:0] state;
+reg [2:0] next_state;
+reg       increm;
+
+localparam IDLE        = 3'd0;
+localparam CHANGING_H  = 3'd1;
+localparam CHANGING_TS = 3'd2;
+localparam CHANGING_S  = 3'd3;
+localparam CHANGING_T  = 3'd4;
+
+assign state_value_o = state;
+
+always @( * ) begin
+  next_state = state;
+  increm     = 1'b0;
+  case( state )
+    IDLE:
+      begin
+        if( !dev_run_i )
+          if( set_i & !start_i )
+            next_state = CHANGING_H;
+      end
+
+    CHANGING_H:
+      begin
+        if( !dev_run_i ) begin
+          if( set_i & !start_i )
+            next_state = CHANGING_TS;
+          if( change_i )
+            increm = 1'b1;
+          else
+            increm = 1'b0;
+        end
+        else
+          next_state = IDLE;
+      end
+
+    CHANGING_TS:
+      begin
+        if( !dev_run_i ) begin
+          if( set_i & !start_i )
+            next_state = CHANGING_S;
+          if( change_i )
+            increm = 1'b1;
+          else
+            increm = 1'b0;
+        end
+        else
+          next_state = IDLE;
+      end
+
+    CHANGING_S:
+      begin
+        if( !dev_run_i ) begin
+          if( set_i & !start_i )
+            next_state = CHANGING_T;
+          if( change_i )
+            increm = 1'b1;
+          else
+            increm = 1'b0;
+        end
+        else
+          next_state = IDLE;
+      end
+
+    CHANGING_T:
+      begin
+        if( !dev_run_i ) begin
+          if( set_i & !start_i ) begin
+            next_state = IDLE;
+          end
+          if( change_i )
+            increm = 1'b1;
+          else
+            increm = 1'b0;
+        end
+        else
+          next_state = IDLE;
+    end
+
+    default:
+      begin
+        next_state = IDLE;
+      end
+  endcase
+end
+
+assign inc_this_o = increm;
+assign cnt        = state;
+
+always @( posedge clk_i or negedge rstn_i ) begin
+  if( !rstn_i )
+    state <= IDLE;
+  else
+    state <= next_state;
+end
+
+endmodule 
--- lab3/rtl/stopwatch.v
+++ lab3/rtl/stopwatch.v
@@ -1,73 +1,54 @@
-`timescale 1ns / 1ps
-
-module stopwatch(
-  input        clk100_i,
+input        clk100_i,
   input        rstn_i,
   input        start_stop_i,
   input        set_i,
   input        change_i,
-  output [6:0] hex0_o, //индикатор сотых долей секунды
-  output [6:0] hex1_o, //индикатор десытых долей секунды
-  output [6:0] hex2_o, //индикатор секунд
-  output [6:0] hex3_o  //индикатор десятков секунд
+  output [6:0] hex0_o,
+  output [6:0] hex1_o,
+  output [6:0] hex2_o,
+  output [6:0] hex3_o
 );
 
-wire start_down;
-wire change_down;
-wire set_down;
+localparam IDLE        = 3'd0;
+localparam CHANGING_H  = 3'd1;
+localparam CHANGING_TS = 3'd2;
+localparam CHANGING_S  = 3'd3;
+localparam CHANGING_T  = 3'd4;
 
-button_deb bt0(
-  .btn_i   ( !start_stop_i ),
-  .ondn_o  ( start_down    ),
-  .rst_i   ( rstn_i        ),
-  .clk_i   ( clk100_i      )
-);
+wire       start_down;
+wire       change_down;
+wire       set_down;
+wire [2:0] fsm_state;
+wire       increment;
+wire [3:0] cnt;
 
-button_deb bt1(
-  .btn_i   ( !set_i   ),
-  .ondn_o  ( set_down ),
-  .rst_i   ( rstn_i   ),
-  .clk_i   ( clk100_i )
-);
-
-button_deb bt3(
-  .btn_i   ( !change_i   ),
-  .ondn_o  ( change_down ),
-  .rst_i   ( rstn_i      ),
-  .clk_i   ( clk100_i    )
-);
-
-reg        device_run;
-wire       device_stop;
-reg  [3:0] cnt;
-
-assign device_stop = ( device_run ) ? 1'b0 : 1'b1;
+reg        device_running;
 
 always @( posedge clk100_i or negedge rstn_i ) begin
   if( !rstn_i )
-    device_run = 1'b0;
+    device_running = 1'b0;
   else
     begin
-    if( start_down & device_run )
-      device_run = 1'b0;
+    if( start_down & device_running )
+      device_running = 1'b0;
     else
-      if( start_down & device_stop & cnt == 0)
-        device_run = 1'b1;
+      if( start_down & !device_running & cnt == 0 )
+        device_running = 1'b1;
     end
 end
 
-reg  [16:0] pulse_counter;
+reg  [18:0] pulse_counter;
 wire        hundredth_of_second_passed;
 
-assign hundredth_of_second_passed = ( pulse_counter == 17'd259999 );
+assign hundredth_of_second_passed = ( pulse_counter == 19'd49999 );
 
 always @( posedge clk100_i or negedge rstn_i ) begin
   if( !rstn_i )
-    pulse_counter <= 17'd0;
+    pulse_counter <= 19'd0;
   else
-    if( device_run | hundredth_of_second_passed )
+    if( device_running | hundredth_of_second_passed )
       if( hundredth_of_second_passed )
-        pulse_counter <= 17'd0;
+        pulse_counter <= 19'd0;
       else
         pulse_counter <= pulse_counter + 1;
 end
@@ -81,19 +62,17 @@ always @( posedge clk100_i or negedge rstn_i ) begin
   if( !rstn_i )
     hundredths_counter <= 4'd0;
   else
-    if( !set_down & change_down & device_stop ) begin
-      if( cnt == 1 & ( hundredths_counter < 4'd9 ) )
+    if( hundredth_of_second_passed )
+      if( tens_of_second_passed )
+        hundredths_counter <= 4'd0;
+      else
         hundredths_counter <= hundredths_counter + 1;
-      else
-        if( cnt == 1 )
-          hundredths_counter <= 0;
-    end
-      else
-        if( hundredth_of_second_passed )
-          if( tens_of_second_passed )
-            hundredths_counter <= 4'd0;
-          else
-            hundredths_counter <= hundredths_counter + 1;
+    else
+      if( fsm_state == CHANGING_H & increment )
+        if( hundredths_counter == 4'd9 )
+          hundredths_counter <= 4'd0;
+        else
+          hundredths_counter <= hundredths_counter + 1;
 end
 
 reg [3:0] tenths_counter;
@@ -105,19 +84,17 @@ always @( posedge clk100_i or negedge rstn_i ) begin
   if( !rstn_i )
     tenths_counter <= 4'd0;
   else
-    if( !set_down & change_down & device_stop ) begin
-      if( cnt == 2 & tenths_counter < 4'd9 )
+    if( tens_of_second_passed )
+      if( second_passed )
+        tenths_counter <= 4'd0;
+      else
         tenths_counter <= tenths_counter + 1;
-      else
-        if( cnt == 2 )
-          tenths_counter <= 0;
-    end
-      else
-        if( tens_of_second_passed )
-          if( second_passed )
-            tenths_counter <= 4'd0;
-          else
-            tenths_counter <= tenths_counter + 1;
+    else
+      if( fsm_state == CHANGING_TS & increment )
+        if( tenths_counter == 4'd9 )
+          tenths_counter <= 4'd0;
+        else
+          tenths_counter <= tenths_counter + 1;
 end
 
 reg  [3:0] seconds_counter;
@@ -129,19 +106,17 @@ always @( posedge clk100_i or negedge rstn_i ) begin
   if( !rstn_i )
     seconds_counter <= 4'd0;
   else
-    if( !set_down & change_down & device_stop ) begin
-      if( cnt == 3 & seconds_counter < 4'd9 )
+    if( second_passed )
+      if( ten_seconds_passed )
+        seconds_counter <= 4'd0;
+      else
         seconds_counter <= seconds_counter + 1;
-      else
-        if( cnt == 3 )
-          seconds_counter <= 0;
-    end
-      else
-        if( second_passed )
-          if( ten_seconds_passed )
-            seconds_counter <= 4'd0;
-          else
-            seconds_counter <= seconds_counter + 1;
+    else
+      if( fsm_state == CHANGING_S & increment )
+        if( seconds_counter == 4'd9 )
+          seconds_counter <= 4'd0;
+        else
+          seconds_counter <= seconds_counter + 1;
 end
 
 reg [3:0] ten_seconds_counter;
@@ -150,33 +125,17 @@ always @( posedge clk100_i or negedge rstn_i ) begin
   if( !rstn_i )
     ten_seconds_counter <= 4'd0;
   else
-    if( !set_down & change_down & device_stop ) begin
-      if( cnt == 4 & ten_seconds_counter < 4'd9 )
+    if( ten_seconds_passed )
+      if( ten_seconds_counter == 4'd9 )
+        ten_seconds_counter <= 4'd0;
+      else
         ten_seconds_counter <= ten_seconds_counter + 1;
-      else
-        if( cnt == 4 )
-          ten_seconds_counter <= 0;
-    end
-      else
-        if( ten_seconds_passed )
-          if( ten_seconds_counter == 4'd9 )
-            ten_seconds_counter <= 4'd0;
-          else
-            ten_seconds_counter <= ten_seconds_counter + 1;
-end
-
-always @( posedge clk100_i or negedge rstn_i ) begin
-  if( !rstn_i ) begin
-    cnt <= 4'd0;
-  end
-  else
-    if( device_stop ) begin
-      if( !start_down & set_down & cnt < 4 )
-        cnt <= cnt + 1;
-      else
-        if( set_down & cnt == 4 )
-          cnt <= 4'd0;
-    end
+    else
+      if( fsm_state == CHANGING_T & increment )
+        if( ten_seconds_counter == 4'd9 )
+          ten_seconds_counter <= 4'd0;
+        else
+          ten_seconds_counter <= ten_seconds_counter + 1;
 end
 
 dec_hex dec3(
@@ -199,4 +158,37 @@ dec_hex dec0(
   .out( hex0_o             )
 );
 
-endmodule
+button_debounce bt0(
+  .btn_i   ( !start_stop_i ),
+  .ondn_o  ( start_down    ),
+  .rst_i   ( rstn_i        ),
+  .clk_i   ( clk100_i      )
+);
+
+button_debounce bt1(
+  .btn_i   ( !set_i   ),
+  .ondn_o  ( set_down ),
+  .rst_i   ( rstn_i   ),
+  .clk_i   ( clk100_i )
+);
+
+button_debounce bt3(
+  .btn_i   ( !change_i   ),
+  .ondn_o  ( change_down ),
+  .rst_i   ( rstn_i      ),
+  .clk_i   ( clk100_i    )
+);
+
+finstate editmode(
+  .clk_i        ( clk100_i       ),
+  .rstn_i       ( rstn_i         ),
+  .dev_run_i    ( device_running ),
+  .set_i        ( set_down       ),
+  .start_i      ( start_down     ),
+  .change_i     ( change_down    ),
+  .state_value_o( fsm_state      ),
+  .inc_this_o   ( increment      ),
+  .cnt          ( cnt            )
+);
+
+endmodule 
