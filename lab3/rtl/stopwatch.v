b8ccfe8a80f5e31965095b432754ad5a9a8d3619
capython59 <62896832+capython59@users.noreply.github.com>
2020-05-25 18:40:00 +0300

    Конечные доработки lab_3
    Lomakin Georgiy

--- lab3/rtl/stopwatch.v
+++ lab3/rtl/stopwatch.v
@@ -1,40 +1,40 @@
 `timescale 1ns / 1ps
 
 module stopwatch(
-    input        clk100_i,
-    input        rstn_i,
-    input        start_stop_i,
-    input        set_i,
-    input        change_i,
-    output [6:0] hex0_o,
-    output [6:0] hex1_o,
-    output [6:0] hex2_o,
-    output [6:0] hex3_o
+  input        clk100_i,
+  input        rstn_i,
+  input        start_stop_i,
+  input        set_i,
+  input        change_i,
+  output [6:0] hex0_o,
+  output [6:0] hex1_o,
+  output [6:0] hex2_o,
+  output [6:0] hex3_o
 );
 
-localparam PULSE_LIMIT = 20'd9999;
-localparam HUNDREDTH_LIMIT = 4'd9;
-localparam TENTH_LIMIT = 4'd9;
-localparam SECONDS_LIMIT = 4'd9;
+localparam PULSE_LIMIT         = 20'd9999;
+localparam HUNDREDTH_LIMIT     = 4'd9;
+localparam TENTH_LIMIT         = 4'd9;
+localparam SECONDS_LIMIT       = 4'd9;
 localparam TENTH_SECONDS_LIMIT = 4'd9;
 
 //start-stop button logic desc
-reg [2:0] button_synchroniser;
-wire      button_was_pressed;
+reg [2:0] start_stop_synchroniser;
+wire      start_stop_was_pressed;
 
 always @( posedge clk100_i or negedge rstn_i )
   begin
     if ( ~rstn_i )
-      button_synchroniser <= 3'd0;
+      start_stop_synchroniser      <= 3'd0;
     else
       begin
-        button_synchroniser[0] <= ~start_stop_i;
-        button_synchroniser[1] <= button_synchroniser[0];
-        button_synchroniser[2] <= button_synchroniser[1];
+       start_stop_synchroniser[0]  <= ~start_stop_i;
+       start_stop_synchroniser[1]  <=  start_stop_synchroniser[0];
+       start_stop_synchroniser[2]  <=  start_stop_synchroniser[1];
       end
   end
 
-assign button_was_pressed =  ~button_synchroniser[2]  &  button_synchroniser[1];
+assign start_stop_was_pressed =  ~start_stop_synchroniser[2]  &  start_stop_synchroniser[1];
 
 //set button desc
 reg [2:0] set_synchroniser;
@@ -43,12 +43,12 @@ wire      set_was_pressed;
 always @( posedge clk100_i or negedge rstn_i )
   begin
     if ( ~rstn_i )
-      set_synchroniser <= 3'd0;
+      set_synchroniser      <= 3'd0;
     else
       begin
         set_synchroniser[0] <= ~set_i;
-        set_synchroniser[1] <= set_synchroniser[0];
-        set_synchroniser[2] <= set_synchroniser[1];
+        set_synchroniser[1] <=  set_synchroniser[0];
+        set_synchroniser[2] <=  set_synchroniser[1];
       end
   end
 
@@ -61,7 +61,7 @@ wire      change_was_pressed;
 always @( posedge clk100_i or negedge rstn_i )
   begin
     if ( ~rstn_i )
-      change_synchroniser <= 3'd0;
+      change_synchroniser      <= 3'd0;
     else
       begin
         change_synchroniser[0] <= ~change_i;
@@ -75,10 +75,8 @@ assign change_was_pressed =  ~change_synchroniser[2]  &  change_synchroniser[1];
 reg device_running;
 always @( posedge clk100_i or negedge rstn_i )
   begin
-    if ( ~rstn_i )
-      device_running <= 0;
-    else if ( button_was_pressed & ( state_adjust == STATE_DEFAULT ) )
-      device_running <= ~device_running;
+    if ( ~rstn_i )                                                        device_running <= 0;
+    else if ( start_stop_was_pressed & ( state_mode == STATE_DEFAULT ) )  device_running <= ~device_running;
   end
 
 reg [19:0] pulse_counter;
@@ -86,154 +84,132 @@ wire       hundredth_of_second_passed = ( pulse_counter == PULSE_LIMIT );
   
 always @( posedge clk100_i or negedge rstn_i )
   begin
-    if ( ~rstn_i ) 
-      pulse_counter <= 0;
-    else if ( device_running |
-              hundredth_of_second_passed )
+    if ( ~rstn_i )                         pulse_counter <= 0;
+    else if ( device_running | hundredth_of_second_passed )
       begin
-        if ( hundredth_of_second_passed )
-          pulse_counter <= 0;
-        else
-          pulse_counter <= pulse_counter + 1;
+        if ( hundredth_of_second_passed )  pulse_counter <= 0;
+        else                               pulse_counter <= pulse_counter + 1;
       end;
   end;
   
 //digits desc
 reg [3:0] hundredth_counter = 4'd0;
-wire      tenth_of_second_passed = 
-  ( ( hundredth_counter == HUNDREDTH_LIMIT ) &
-      hundredth_of_second_passed );
+wire      tenth_of_second_passed = ( ( hundredth_counter == HUNDREDTH_LIMIT ) &  
+                                       hundredth_of_second_passed           );
       
 always @( posedge clk100_i or negedge rstn_i )
   begin
-    if ( ~rstn_i )
-      hundredth_counter <= 0;
+    if      ( ~rstn_i )                hundredth_counter <= 0;
     else if ( hundredth_of_second_passed )
       begin
-        if ( tenth_of_second_passed )
-          hundredth_counter <= 0;
-        else
-          hundredth_counter <= hundredth_counter + 1;
+        if ( tenth_of_second_passed )  hundredth_counter <= 0;
+        else                           hundredth_counter <= hundredth_counter + 1;
       end;
   end;
       
 reg [3:0] tenth_counter = 4'd0;
-wire      second_passed = 
-  ( ( tenth_counter == TENTH_LIMIT ) &
-      tenth_of_second_passed );
+wire      second_passed = ( ( tenth_counter == TENTH_LIMIT ) &
+                              tenth_of_second_passed       );
       
 always @( posedge clk100_i or negedge rstn_i )
   begin
-    if ( ~rstn_i )
-      tenth_counter <= 0;
+    if ( ~rstn_i )            tenth_counter <= 0;
     else if ( tenth_of_second_passed )
       begin
-        if ( second_passed )
-          tenth_counter <= 0;
-        else 
-          tenth_counter <= tenth_counter + 1;
+        if ( second_passed )  tenth_counter <= 0;
+        else                  tenth_counter <= tenth_counter + 1;
       end;
   end;      
   
 reg [3:0] second_counter = 4'd0;
-wire      tenth_second_passed = 
-  ( ( second_counter == SECONDS_LIMIT ) &
-      second_passed );
+wire      tenth_second_passed = ( ( second_counter == SECONDS_LIMIT ) & 
+                                    second_passed                   );
       
 always @( posedge clk100_i or negedge rstn_i )
   begin
-    if ( !rstn_i )
-      second_counter <= 0;
+    if ( !rstn_i )  second_counter <= 0;
     else if ( second_passed )
       begin
-        if ( tenth_second_passed )
-          second_counter <= 0;
-        else 
-          second_counter <= second_counter + 1;
+        if ( tenth_second_passed )  second_counter <= 0;
+        else                        second_counter <= second_counter + 1;
       end;
   end;   
       
 reg [3:0] tenth_second_counter = 4'd0;
-wire      minute_passed = 
-  ( ( tenth_second_counter == 4'd9 ) &
-      tenth_second_passed );      
+wire      minute_passed = ( ( tenth_second_counter == 4'd9 ) &
+                              tenth_second_passed          );      
       
 always @( posedge clk100_i or negedge rstn_i )
   begin
-    if ( ~rstn_i )
-      tenth_second_counter <= 0;
+    if ( ~rstn_i )  tenth_second_counter <= 0;
     else if ( tenth_second_passed )
       begin
-        if ( tenth_second_counter == TENTH_SECONDS_LIMIT )
-          tenth_second_counter <= 0;
-        else 
-          tenth_second_counter <= tenth_second_counter + 1;
+        if ( tenth_second_counter == TENTH_SECONDS_LIMIT )  tenth_second_counter <= 0;
+        else                                                tenth_second_counter <= tenth_second_counter + 1;
       end;
   end;   
 
 //Finite-state machine logic desc          
- reg [2:0] state_adjust;
- reg [2:0] next_state_adjust;
+ reg [2:0] state_mode;
+ reg [2:0] next_state_mode;
  
  localparam STATE_DEFAULT = 3'd0;
- localparam STATE_D_1     = 3'd1;
- localparam STATE_D_2     = 3'd2;
- localparam STATE_D_3     = 3'd3;
- localparam STATE_D_4     = 3'd4;
+ localparam STATE_1     = 3'd1;
+ localparam STATE_2     = 3'd2;
+ localparam STATE_3     = 3'd3;
+ localparam STATE_4     = 3'd4;
  
  always @( * ) 
    begin
-     case ( state_adjust )
-       STATE_DEFAULT : if ( ( ~device_running ) & ( set_was_pressed ) )  next_state_adjust = STATE_D_1;
-                       else                                              next_state_adjust = STATE_DEFAULT;
+     case ( state_mode )
+       STATE_DEFAULT : if ( ( ~device_running ) & ( set_was_pressed ) )  next_state_mode = STATE_1;
+                       else                                              next_state_mode = STATE_DEFAULT;
                       
-       STATE_D_1 : if ( set_was_pressed ) next_state_adjust = STATE_D_2;
-                   else if ( change_was_pressed ) 
-                     begin
-                       if ( hundredth_counter == HUNDREDTH_LIMIT )    hundredth_counter = 4'd0;
-                       else                                           hundredth_counter = hundredth_counter + 1;
-                        next_state_adjust = STATE_D_1;
-                     end
-                   else next_state_adjust = STATE_D_1;
+       STATE_1 : if ( set_was_pressed ) next_state_mode = STATE_2;
+                 else if ( change_was_pressed ) 
+                   begin
+                     if ( hundredth_counter == HUNDREDTH_LIMIT )    hundredth_counter = 4'd0;
+                     else                                           hundredth_counter = hundredth_counter + 1;
+                     next_state_mode =  STATE_1;
+                   end
+                 else next_state_mode = STATE_1;
                      
-       STATE_D_2 : if ( set_was_pressed ) next_state_adjust = STATE_D_3;
-                   else if ( change_was_pressed ) 
-                     begin
-                       if ( tenth_counter == TENTH_LIMIT )            tenth_counter = 4'd0;
-                       else                                           tenth_counter = tenth_counter + 1;
-                        next_state_adjust = STATE_D_2;
-                     end
-                   else next_state_adjust = STATE_D_2;
+       STATE_2 : if ( set_was_pressed ) next_state_mode = STATE_3;
+                 else if ( change_was_pressed ) 
+                   begin
+                     if ( tenth_counter == TENTH_LIMIT )            tenth_counter = 4'd0;
+                     else                                           tenth_counter = tenth_counter + 1;
+                     next_state_mode =  STATE_2;
+                   end
+                 else next_state_mode = STATE_2;
                    
-       STATE_D_3 : if ( set_was_pressed ) next_state_adjust = STATE_D_4;
-                   else if ( change_was_pressed ) 
-                     begin
-                       if ( second_counter == SECONDS_LIMIT )         second_counter = 4'd0;
-                       else                                           second_counter = second_counter + 1;
-                        next_state_adjust = STATE_D_3;
-                     end
-                   else next_state_adjust = STATE_D_3;
+       STATE_3 : if ( set_was_pressed ) next_state_mode = STATE_4;
+                 else if ( change_was_pressed ) 
+                   begin
+                     if ( second_counter == SECONDS_LIMIT )         second_counter = 4'd0;
+                     else                                           second_counter = second_counter + 1;
+                     next_state_mode =  STATE_3;
+                   end
+                 else next_state_mode = STATE_3;
                    
-       STATE_D_4 : if ( set_was_pressed ) next_state_adjust = STATE_DEFAULT;
-                   else if ( change_was_pressed ) 
-                     begin
-                       if ( tenth_second_counter == HUNDREDTH_LIMIT ) tenth_second_counter = 4'd0;
-                       else                                           tenth_second_counter = tenth_second_counter + 1;
-                        next_state_adjust = STATE_D_4;
-                     end
-                   else next_state_adjust = STATE_D_4;
+       STATE_4 : if ( set_was_pressed ) next_state_mode = STATE_DEFAULT;
+                 else if ( change_was_pressed ) 
+                   begin
+                     if ( tenth_second_counter == HUNDREDTH_LIMIT ) tenth_second_counter = 4'd0;
+                     else                                           tenth_second_counter = tenth_second_counter + 1;
+                     next_state_mode =  STATE_4;
+                   end
+                 else next_state_mode = STATE_4;
                    
-       default   :      next_state_adjust = STATE_DEFAULT;
+       default   :   next_state_mode = STATE_DEFAULT;
      endcase
    end
  
- //state adjust logic desc
+ //state mode logic desc
  always @( posedge clk100_i or negedge rstn_i )
    begin
-     if ( !rstn_i )
-       state_adjust <= STATE_DEFAULT;
-     else
-       state_adjust <= next_state_adjust;
+     if ( !rstn_i )  state_mode <= STATE_DEFAULT;
+     else            state_mode <= next_state_mode;
    end
 
 //hex0-hex3 desc   
--- lab3/tb/stopwatch_tb.v
+++ lab3/tb/stopwatch_tb.v
@@ -1,116 +1,177 @@
 `timescale 1ns / 1ps
 
-module stopwatch_tb();
+module stopwatch_tb(
+
+  );
+
+localparam CLK_FREQ_MHZ = 100;
+localparam CLK_SEMIPERIOD = (1000/CLK_FREQ_MHZ/2);
+
+  reg          clk100_i;
+  reg          rstn_i;
+  reg          start_stop_i;
+  reg          set_i;
+  reg          change_i;
+  wire  [6:0]  hex0_o;
+  wire  [6:0]  hex1_o;
+  wire  [6:0]  hex2_o;
+  wire  [6:0]  hex3_o;
+
+  stopwatch DUT (
+    .clk100_i      ( clk100_i     ),
+    .rstn_i        ( rstn_i       ),
+    .start_stop_i  ( start_stop_i ),
+    .set_i         ( set_i        ),
+    .change_i      ( change_i     ),
+    .hex0_o        ( hex0_o       ),
+    .hex1_o        ( hex1_o       ),
+    .hex2_o        ( hex2_o       ),
+    .hex3_o        ( hex3_o       )
+  );
+
+  initial begin
+    rstn_i = 1'b1;
+    start_stop_i = 1'b1;
+    set_i = 1'b1;
+    change_i = 1'b1;
     
-localparam CLK_FREQ_MHZ   = 100;
-localparam CLK_SEMIPERIOD = ( 1000 / CLK_FREQ_MHZ) / 2;       
+    #100
+    set_i = 1'b0;
+    #100
+    set_i = 1'b1;
     
-reg        clk100;
-reg        rstn;
-reg        start_stop;
-reg        set;
-reg        change;
-wire [6:0] hex0;
-wire [6:0] hex1;
-wire [6:0] hex2;
-wire [6:0] hex3;
-
-stopwatch DUT (
-  .clk100_i     ( clk100     ),
-  .rstn_i       ( rstn       ),
-  .start_stop_i ( start_stop ),
-  .set_i        ( set        ),
-  .change_i     ( change     ),
-  .hex0_o       ( hex0       ),
-  .hex1_o       ( hex1       ),
-  .hex2_o       ( hex2       ),
-  .hex3_o       ( hex3       )
-);
-
-initial begin
-  clk100 = 1'b1;
-  forever begin
-    #CLK_SEMIPERIOD clk100 = ~clk100;
-  end
-end
-
-initial begin
-  rstn = 1;
-  #( 4*CLK_SEMIPERIOD );
-  rstn = 0;
-  #( 4*CLK_SEMIPERIOD );
-  rstn = 1;
-end
-
-initial begin
-  start_stop = 1'b1;
-  set        = 1'b1;
-  change     = 1'b1;
- 
-  @( posedge rstn );
-  set = 1'b0;
-  #( 10*CLK_SEMIPERIOD );
-  set = 1'b1;
-  
-  #( 10*CLK_SEMIPERIOD );
-  change = 1'b0;
-  #( 10*CLK_SEMIPERIOD );
-  change = 1'b1;
-  
-  #( 10*CLK_SEMIPERIOD );
-  set = 1'b0;
-  #( 10*CLK_SEMIPERIOD );
-  set = 1'b1;
-  
-  repeat ( 9 ) begin
-    #( 10*CLK_SEMIPERIOD );
-    change = 1'b0;
-    #( 10*CLK_SEMIPERIOD );
-    change = 1'b1;
-  end
-  
-  #( 10*CLK_SEMIPERIOD );
-  set = 1'b0;
-  #( 10*CLK_SEMIPERIOD );
-  set = 1'b1;
-  
-  repeat ( 8 ) begin
-    #( 10*CLK_SEMIPERIOD );
-    change = 1'b0;
-    #( 10*CLK_SEMIPERIOD );
-    change = 1'b1;
-  end
-  
-  #( 10*CLK_SEMIPERIOD );
-  set = 1'b0;
-  #( 10*CLK_SEMIPERIOD );
-  set = 1'b1;
-  
-  repeat ( 4 ) begin
-    #( 10*CLK_SEMIPERIOD );
-    change = 1'b0;
-    #( 10*CLK_SEMIPERIOD );
-    change = 1'b1;
-  end
-  
-  #( 10*CLK_SEMIPERIOD );
-  set = 1'b0;
-  #( 10*CLK_SEMIPERIOD );
-  set = 1'b1;
-  
-  #( 20*CLK_SEMIPERIOD );
-  repeat ( 20 ) begin
-    start_stop = 1'b0;
-    #( 10*CLK_SEMIPERIOD );
-    start_stop = 1'b1;
-    #( 1000000*2*CLK_SEMIPERIOD );
-    start_stop = 1'b0;
-    #( 10*CLK_SEMIPERIOD );
-    start_stop = 1'b1;
-  end
-  
-end
-
-
+    //00.01
+    #1000
+    change_i = 1'b0;
+    #100
+    change_i = 1'b1;
     
+    #1000
+    set_i = 1'b0;   
+    #100            
+    set_i = 1'b1;   
+     
+    //00.10            
+    
+    
+    #1000           
+    change_i = 1'b0;
+    #100            
+    change_i = 1'b1;
+    
+    #1000           
+    change_i = 1'b0;
+    #100            
+    change_i = 1'b1;
+    
+    #1000           
+    change_i = 1'b0;
+    #100            
+    change_i = 1'b1;
+    
+    #1000           
+    change_i = 1'b0;
+    #100            
+    change_i = 1'b1;
+    
+    #1000           
+    change_i = 1'b0;
+    #100            
+    change_i = 1'b1;
+    
+    
+    #1000           
+    change_i = 1'b0;
+    #100            
+    change_i = 1'b1;
+    
+    #1000           
+    change_i = 1'b0;
+    #100            
+    change_i = 1'b1;
+    
+    #1000           
+    change_i = 1'b0;
+    #100            
+    change_i = 1'b1;
+    
+    #1000           
+    change_i = 1'b0;
+    #100            
+    change_i = 1'b1;
+    
+    #1000           
+    change_i = 1'b0;
+    #100            
+    change_i = 1'b1;
+    
+    #1000           
+    change_i = 1'b0;
+    #100            
+    change_i = 1'b1;
+    
+    #1000           
+    change_i = 1'b0;
+    #100            
+    change_i = 1'b1;
+    
+    #1000           
+    change_i = 1'b0;
+    #100            
+    change_i = 1'b1;
+    
+    #1000           
+    change_i = 1'b0;
+    #100            
+    change_i = 1'b1;
+    
+    #1000           
+    change_i = 1'b0;
+    #100            
+    change_i = 1'b1;
+    
+    #100
+    set_i = 1'b0;
+    #100
+    set_i = 1'b1;
+    
+    //01.00
+    #1000
+    change_i = 1'b0;
+    #100
+    change_i = 1'b1;
+    
+    #1000
+    set_i = 1'b0;   
+    #100            
+    set_i = 1'b1;   
+    
+    //10.00             
+    #1000           
+    change_i = 1'b0;
+    #100            
+    change_i = 1'b1;
+    
+    #100000
+    set_i = 1'b0;   
+    #100            
+    set_i = 1'b1;  
+    
+    #1000
+    start_stop_i = 1'b0;
+    #100
+    start_stop_i = 1'b1;
+    
+    #10000
+    change_i = 1'b0;
+    #100
+    change_i = 1'b1;
+    
+  end
+
+  initial begin
+    clk100_i = 1'b0;
+    forever #CLK_SEMIPERIOD clk100_i = ~clk100_i;  
+  end
+  
 endmodule
