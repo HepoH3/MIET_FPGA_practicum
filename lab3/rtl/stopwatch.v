e6f37f12aa2ca6e0ec2af6eeba9d594df6aece1e
Nikita <nikita-gon1@mail.ru>
2020-05-25 10:17:32 +0300

    final version

--- lab3/rtl/stopwatch.v
+++ lab3/rtl/stopwatch.v
@@ -1,156 +1,200 @@
-`timescale 1ns / 1ps
-
 module stopwatch(
-    input clk100_i,
-    input rstn_i,
-    input start_stop_i,
-    input set_i,
-    input change_i,
-    output [6:0] hex0_o, //индикатор сотых долей секунды
-    output [6:0] hex1_o, //индикатор десытых долей секунды
-    output [6:0] hex2_o, //индикатор секунд
-    output [6:0] hex3_o  //индикатор десятков секунд
+  input        clk100_i,
+  input        rstn_i,
+  input        start_stop_i,
+  input        set_i,
+  input        change_i,
+  output [6:0] hex0_o,
+  output [6:0] hex1_o,
+  output [6:0] hex2_o,
+  output [6:0] hex3_o
 );
 
-// Часть 1
-// нажатие кнопки "Старт/Стоп"
-reg [2:0] button_syncroniser;
-wire button_was_pressed;
-always @( posedge clk100_i ) begin
-  button_syncroniser[0] <= start_stop_i;
-  button_syncroniser[1] <= button_syncroniser[0];
-  button_syncroniser[2] <= button_syncroniser[1];
+wire start_down;
+wire change_down;
+wire set_down;
+
+button_deb bt0(
+  .btn_i   ( !start_stop_i ),
+  .ondn_o  ( start_down    ),
+  .rst_i   ( rstn_i        ),
+  .clk_i   ( clk100_i      )
+);
+
+button_deb bt1(
+  .btn_i   ( !set_i   ),
+  .ondn_o  ( set_down ),
+  .rst_i   ( rstn_i   ),
+  .clk_i   ( clk100_i )
+);
+
+button_deb bt3(
+  .btn_i   ( !change_i   ),
+  .ondn_o  ( change_down ),
+  .rst_i   ( rstn_i      ),
+  .clk_i   ( clk100_i    )
+);
+
+reg        device_running;
+wire       device_stopped;
+reg  [3:0] cnt;
+
+assign device_stopped = ( device_running ) ? 1'b0 : 1'b1;
+
+always @( posedge clk100_i or negedge rstn_i ) begin
+  if( !rstn_i )
+    device_running = 1'b0;
+  else
+    begin
+    if( start_down & device_running )
+      device_running = 1'b0;
+    else
+      if( start_down & device_stopped & cnt == 0)
+        device_running = 1'b1;
+    end
 end
 
-assign button_was_pressed = ~button_syncroniser[2] & button_syncroniser[1];
+reg  [16:0] pulse_counter;
+wire        hundredth_of_second_passed;
 
-//Часть 2
-//Самостоятельная работа студента
-reg device_running;
+assign hundredth_of_second_passed = ( pulse_counter == 17'd259999 );
 
-
-//Часть 3
-// Счётчик импульсов и признак истечения 0.01 сек
-reg [16:0] pulse_counter = 17'd0;
-wire hundredth_of_second_passed = (pulse_counter == 17'd259999);
-always @(posedge clk100_i or posedge rstn_i) begin
-  if ( rstn_i ) pulse_counter <= 0;
-  else if ( device_running | hundredth_of_second_passed)
-    if (hundredth_of_second_passed)
-      pulse_counter <= 0;
-    else pulse_counter <= pulse_counter + 1;
+always @( posedge clk100_i or negedge rstn_i ) begin
+  if( !rstn_i )
+    pulse_counter <= 17'd0;
+  else
+    if( device_running | hundredth_of_second_passed )
+      if( hundredth_of_second_passed )
+        pulse_counter <= 17'd0;
+      else
+        pulse_counter <= pulse_counter + 1;
 end
 
-// Часть 4 - основные счётчики
-reg [3:0]  hundredth_counter = 4'd0;
-wire tenth_of_second_passed = ((hundredth_counter == 4'd9) & hundredth_of_second_passed);
-always @(posedge clk100_i or posedge rstn_i) begin
-  if ( rstn_i ) hundredth_counter <= 0;
-  else if ( hundredth_of_second_passed )
-    if ( tenth_of_second_passed )
-      hundredth_counter <= 0;
-    else hundredth_counter <= hundredth_counter + 1;
+reg  [3:0] hundredths_counter;
+wire       tens_of_second_passed;
+
+assign tens_of_second_passed = ( ( hundredths_counter == 4'd9 ) & hundredth_of_second_passed );
+
+always @( posedge clk100_i or negedge rstn_i ) begin
+  if( !rstn_i )
+    hundredths_counter <= 4'd0;
+  else
+    if( !set_down & change_down & device_stopped ) begin
+      if( cnt == 1 & ( hundredths_counter < 4'd9 ) )
+        hundredths_counter <= hundredths_counter + 1;
+      else
+        if( cnt == 1 )
+          hundredths_counter <= 0;
+    end
+      else
+        if( hundredth_of_second_passed )
+          if( tens_of_second_passed )
+            hundredths_counter <= 4'd0;
+          else
+            hundredths_counter <= hundredths_counter + 1;
 end
 
-reg [3:0] tenth_counter = 4'd0;
-wire second_passed = ((tenth_counter == 4'd9) & tenth_of_second_passed);
-always @(posedge clk100_i or posedge rstn_i) begin
-  if ( rstn_i ) tenth_counter <= 0;
-  else if ( tenth_of_second_passed )
-    if ( second_passed )
-      tenth_counter <= 0;
-    else tenth_counter <= tenth_counter + 1;
-end
-  
-reg [3:0] second_counter = 4'd0;
-wire tenth_seconds_passed = ((second_counter == 4'd9) & second_passed);
-always @(posedge clk100_i or posedge rstn_i) begin
-  if ( rstn_i ) second_counter <= 0;
-  else if ( second_passed )
-    if ( tenth_seconds_passed )
-      second_counter <= 0;
-    else second_counter <= second_counter + 1;
+reg [3:0] tenths_counter;
+wire      second_passed;
+
+assign second_passed = ( ( tenths_counter == 4'd9 ) & tens_of_second_passed );
+
+always @( posedge clk100_i or negedge rstn_i ) begin
+  if( !rstn_i )
+    tenths_counter <= 4'd0;
+  else
+    if( !set_down & change_down & device_stopped ) begin
+      if( cnt == 2 & tenths_counter < 4'd9 )
+        tenths_counter <= tenths_counter + 1;
+      else
+        if( cnt == 2 )
+          tenths_counter <= 0;
+    end
+      else
+        if( tens_of_second_passed )
+          if( second_passed )
+            tenths_counter <= 4'd0;
+          else
+            tenths_counter <= tenths_counter + 1;
 end
 
-reg [3:0] ten_seconds_counter = 4'd0;
-always @(posedge clk100_i or posedge rstn_i) begin
-  if ( rstn_i ) ten_seconds_counter <= 0;
-  else if ( tenth_seconds_passed )
-    if ( ten_seconds_counter == 4'd9 )
-     ten_seconds_counter <= 0;
-    else ten_seconds_counter <= ten_seconds_counter + 1;
+reg  [3:0] seconds_counter;
+wire       ten_seconds_passed;
+
+assign ten_seconds_passed = ( ( seconds_counter == 4'd9 ) & second_passed );
+
+always @( posedge clk100_i or negedge rstn_i ) begin
+  if( !rstn_i )
+    seconds_counter <= 4'd0;
+  else
+    if( !set_down & change_down & device_stopped ) begin
+      if( cnt == 3 & seconds_counter < 4'd9 )
+        seconds_counter <= seconds_counter + 1;
+      else
+        if( cnt == 3 )
+          seconds_counter <= 0;
+    end
+      else
+        if( second_passed )
+          if( ten_seconds_passed )
+            seconds_counter <= 4'd0;
+          else
+            seconds_counter <= seconds_counter + 1;
 end
 
-//Часть 5 дешифратор
-reg [6:0] decoder_ten_seconds;
-always @(*) begin
-  case (ten_seconds_counter)
-    4'd0:  decoder_ten_seconds <= 7'b0000001;
-    4'd1:  decoder_ten_seconds <= 7'b1001111;
-    4'd2:  decoder_ten_seconds <= 7'b0010010;
-    4'd3:  decoder_ten_seconds <= 7'b0000110;
-    4'd4:  decoder_ten_seconds <= 7'b1001100;
-    4'd5:  decoder_ten_seconds <= 7'b0100100;
-    4'd6:  decoder_ten_seconds <= 7'b0100000;
-    4'd7:  decoder_ten_seconds <= 7'b0001111;
-    4'd8:  decoder_ten_seconds <= 7'b0000000;
-    4'd9:  decoder_ten_seconds <= 7'b0000100;
-    default: decoder_ten_seconds <= 7'b1111111;
-  endcase;
-end
-assign hex3 = decoder_ten_seconds;
-reg [6:0] decoder_seconds;
-always @(*) begin
-  case (second_counter)
-    4'd0:  decoder_seconds <= 7'b0000001;
-    4'd1:  decoder_seconds <= 7'b1001111;
-    4'd2:  decoder_seconds <= 7'b0010010;
-    4'd3:  decoder_seconds <= 7'b0000110;
-    4'd4:  decoder_seconds <= 7'b1001100;
-    4'd5:  decoder_seconds <= 7'b0100100;
-    4'd6:  decoder_seconds <= 7'b0100000;
-    4'd7:  decoder_seconds <= 7'b0001111;
-    4'd8:  decoder_seconds <= 7'b0000000;
-    4'd9:  decoder_seconds <= 7'b0000100;
-    default: decoder_seconds <= 7'b1111111;
-  endcase;
-end
-assign hex2 = decoder_seconds;
+reg [3:0] ten_seconds_counter;
 
-reg [6:0] decoder_tenths;
-always @(*) begin
-  case (tenth_counter)
-    4'd0:  decoder_tenths <= 7'b0000001;
-    4'd1:  decoder_tenths <= 7'b1001111;
-    4'd2:  decoder_tenths <= 7'b0010010;
-    4'd3:  decoder_tenths <= 7'b0000110;
-    4'd4:  decoder_tenths <= 7'b1001100;
-    4'd5:  decoder_tenths <= 7'b0100100;
-    4'd6:  decoder_tenths <= 7'b0100000;
-    4'd7:  decoder_tenths <= 7'b0001111;
-    4'd8:  decoder_tenths <= 7'b0000000;
-    4'd9:  decoder_tenths <= 7'b0000100;
-    default: decoder_tenths <= 7'b1111111;
-  endcase;
+always @( posedge clk100_i or negedge rstn_i ) begin
+  if( !rstn_i )
+    ten_seconds_counter <= 4'd0;
+  else
+    if( !set_down & change_down & device_stopped ) begin
+      if( cnt == 4 & ten_seconds_counter < 4'd9 )
+        ten_seconds_counter <= ten_seconds_counter + 1;
+      else
+        if( cnt == 4 )
+          ten_seconds_counter <= 0;
+    end
+      else
+        if( ten_seconds_passed )
+          if( ten_seconds_counter == 4'd9 )
+            ten_seconds_counter <= 4'd0;
+          else
+            ten_seconds_counter <= ten_seconds_counter + 1;
 end
-assign hex1 = decoder_tenths;
 
-reg [6:0] decoder_hundredths;
-always @(*) begin
-  case (hundredth_counter)
-    4'd0:  decoder_hundredths <= 7'b0000001;
-    4'd1:  decoder_hundredths <= 7'b1001111;
-    4'd2:  decoder_hundredths <= 7'b0010010;
-    4'd3:  decoder_hundredths <= 7'b0000110;
-    4'd4:  decoder_hundredths <= 7'b1001100;
-    4'd5:  decoder_hundredths <= 7'b0100100;
-    4'd6:  decoder_hundredths <= 7'b0100000;
-    4'd7:  decoder_hundredths <= 7'b0001111;
-    4'd8:  decoder_hundredths <= 7'b0000000;
-    4'd9:  decoder_hundredths <= 7'b0000100;
-    default: decoder_hundredths <= 7'b1111111;
-  endcase;
+always @( posedge clk100_i or negedge rstn_i ) begin
+  if( !rstn_i ) begin
+    cnt <= 4'd0;
+  end
+  else
+    if( device_stopped ) begin
+      if( !start_down & set_down & cnt < 4 )
+        cnt <= cnt + 1;
+      else
+        if( set_down & cnt == 4 )
+          cnt <= 4'd0;
+    end
 end
-assign hex0 = decoder_hundredths;
 
-endmodule
+dec_hex dec3(
+  .in ( ten_seconds_counter ),
+  .out( hex3_o              )
+);
+
+dec_hex dec2(
+  .in ( seconds_counter     ),
+  .out( hex2_o              )
+);
+
+dec_hex dec1(
+  .in ( tenths_counter     ),
+  .out( hex1_o             )
+);
+
+dec_hex dec0(
+  .in ( hundredths_counter ),
+  .out( hex0_o             )
+);
+
+endmodule 
